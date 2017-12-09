86059ff032c1da3b559ecf77aefaa038dfd82bed
Michael Daniel <michael.daniel@kattau.at>
2017-12-09 20:27:00 +0100

    Added Powershell v4 compatibility + ImportExcel Module Installation

--- FFSpenden-Helper.ps1
+++ FFSpenden-Helper.ps1
@@ -1,11 +1,123 @@
-﻿#$executionPolicy = Get-ExecutionPolicy
-#Set-ExecutionPolicy -ExecutionPolicy Bypass
+﻿#############################################################################
+## Filename: FFSpenden-Helper.ps1
+## Author:   Michael Daniel
+## E-Mail:   michael.daniel.h@gmail.com
+## Purpose:  Creates a SZR-Upload File based on an Excel-List of donators.
+##           The response SZR Ziparchive can be imported for creating the
+##           Finanzonline XML File.
+## Thanks:   Uses dfinke's awesome ImportExcel module
+##
+#############################################################################
 
-if (-NOT (Get-Module -ListAvailable -Name ImportExcel)){
-    Install-Module ImportExcel -scope CurrentUser
+
+
+#############################################################################
+## Functions
+#############################################################################
+
+# Download of the ImportExcel files if PS4 is used.
+# The provided Install Script is buggy and not functioning, so I adapted it by myself.
+Function Get-ModuleImportExcelFiles() {
+    [CmdLetBinding()]
+    Param (
+        [ValidateNotNullOrEmpty()]
+        [String]$ModuleName = 'ImportExcel',
+        [String]$InstallDirectory,
+        [ValidateNotNullOrEmpty()]
+        [String]$GitPath = 'https://raw.github.com/dfinke/ImportExcel/master'
+    )
+
+    Begin {
+        Try {
+            Write-Verbose "$ModuleName Modulinstallation wurde gestartet"
+
+            $Files = @(
+                'EPPlus.dll',
+                'ImportExcel.psd1',
+                'ImportExcel.psm1',
+                'AddConditionalFormatting.ps1',
+                'Charting.ps1',
+                'ColorCompletion.ps1',
+                'ConvertFromExcelData.ps1',
+                'ConvertFromExcelToSQLInsert.ps1',
+                'ConvertExcelToImageFile.ps1',
+                'ConvertToExcelXlsx.ps1',
+                'Copy-ExcelWorkSheet.ps1',
+                'Export-charts.ps1',
+                'Export-Excel.ps1',
+                'Export-ExcelSheet.ps1',
+                'formatting.ps1',
+                'Get-ExcelColumnName.ps1',
+                'Get-ExcelSheetInfo.ps1',
+                'Get-ExcelWorkbookInfo.ps1',
+                'Get-HtmlTable.ps1',
+                'Get-Range.ps1',
+                'Get-XYRange.ps1',
+                'Import-Html.ps1',
+                'InferData.ps1',
+                'Invoke-Sum.ps1',
+                'New-ConditionalFormattingIconSet.ps1',
+                'New-ConditionalText.ps1',
+                'New-ExcelChart.ps1',
+                'New-PSItem.ps1',
+                'Open-ExcelPackage.ps1',
+                'Pivot.ps1',
+                'plot.ps1',
+                'Send-SqlDataToExcel.ps1',
+                'Set-CellStyle.ps1',
+                'Set-Column.ps1',
+                'Set-Row.ps1',
+                'SetFormat.ps1',
+                'TrackingUtils.ps1',
+                'Update-FirstObjectProperties.ps1'
+            )
+        }
+        Catch {
+            throw "Fehler beim Installieren des Moduls in das Verzeichnis '$InstallDirectory': $_"
+        }
+    }
+
+    Process {
+        Try {
+            if (-not $InstallDirectory) {
+                Write-Verbose "$ModuleName Kein Installationsverzeichnis angegeben"
+
+                $PersonalModules = Join-Path -Path ([Environment]::GetFolderPath('MyDocuments')) -ChildPath WindowsPowerShell\Modules
+
+                if (($env:PSModulePath -split ';') -notcontains $PersonalModules) {
+                    Write-Warning "$ModuleName Persoenlicher Modulpfad '$PersonalModules' nicht gefunden in '`$env:PSModulePath'"
+                }
+
+                if (-not (Test-Path $PersonalModules)) {
+                    Write-Error "$ModuleName Pfad '$PersonalModules' existiert nicht"
+                }
+
+                $InstallDirectory = Join-Path -Path $PersonalModules -ChildPath $ModuleName
+                Write-Verbose "$ModuleName Installationsverzeichnis ist '$InstallDirectory'"
+            }
+
+            if (-not (Test-Path $InstallDirectory)) {
+                New-Item -Path $InstallDirectory -ItemType Directory -EA Stop | Out-Null
+                Write-Verbose "$ModuleName Modul-Ordner wurde erstellt: '$InstallDirectory'"
+            }
+
+            $WebClient = New-Object System.Net.WebClient
+        
+            $Files | ForEach-Object {
+                $WebClient.DownloadFile("$GitPath/$_","$installDirectory\$_")
+                Write-Verbose "$ModuleName Moduldatei erfolgreich installiert: '$_'"
+            }
+
+            Write-Verbose "$ModuleName Modulinstallation erfolgreich abgeschlossen"
+        }
+        Catch {
+            throw "Fehler beim Installieren des Moduls in das Verzeichnis '$InstallDirectory': $_"
+        }
+    }
 }
 
-
+# Get a filename by picking a file from a dialog-box
+# filtering is available
 Function Get-FileName($initialDirectory, $filter = "Excel (*.xlsx)| *.xlsx")
 {
     [System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms") | Out-Null
@@ -17,16 +129,19 @@ Function Get-FileName($initialDirectory, $filter = "Excel (*.xlsx)| *.xlsx")
     return $OpenFileDialog.FileName
 }
 
-Function Show-MessageboxInfo($message, $title, $buttons = [System.Windows.Forms.MessageBoxButtons]::OK)
+# shows a info message box (for informational messages to the user
+Function Show-MessageboxInfo($message, $title, $buttons = [System.Windows.MessageBoxButton]::OK)
 {
-    [System.Windows.MessageBox]::Show($message,$title,$buttons,[System.Windows.Forms.MessageBoxIcon]::Information)
+    [System.Windows.MessageBox]::Show($message,$title,$buttons,'Information')
 }
 
-Function Show-MessageboxError($message, $title, $buttons = [System.Windows.Forms.MessageBoxButtons]::OK)
+# shows a error message box
+Function Show-MessageboxError($message, $title, $buttons = [System.Windows.MessageBoxButton]::OK)
 {
-    [System.Windows.MessageBox]::Show($message,$title,$buttons, [System.Windows.Forms.MessageBoxIcon]::Error)
+    [System.Windows.MessageBox]::Show($message,$title,$buttons, 'Error')
 }
 
+# writes a XML file for uploading in Finanzonline
 Function Write-FoXML($listDonators, $steuernr, $messageRefId, $zeitraum, $xmlFilePath)
 {
     $xmlWriter = New-Object System.Xml.XmlTextWriter($xmlFilePath,$null)
@@ -79,6 +194,8 @@ Function Write-FoXML($listDonators, $steuernr, $messageRefId, $zeitraum, $xmlFil
      
 }
 
+# extract valid donators from an excel worksheet
+# a donator is valid if the firstname, the surname and the birthday is present
 Function Get-ValidDonatorsFromExcelFile($filepath, $sheet, $rowstart)
 {
     $ImportedData = Import-Excel -Path $filepath -WorksheetName $sheet -StartRow $rowstart
@@ -205,7 +322,30 @@ Function Set-vbPKSA($szrAnswerfile, $listDonators) {
     Remove-Item $szrAnswerFileUnzipped -Force -Recurse
 }
 
+#############################################################################
+## actual script start
+#############################################################################
 
+# Install ImportExcel Module - depending on installed PS Version
+if($PSVersionTable.PSVersion.Major -eq 5) {
+    if (-NOT (Get-Module -ListAvailable -Name ImportExcel)){
+        Install-Module ImportExcel -scope CurrentUser
+    }
+} elseif($PSVersionTable.PSVersion.Major -eq 4) {
+    if (-NOT (Get-Module -ListAvailable -Name ImportExcel)) {
+        Get-ModuleImportExcelFiles -Verbose
+    }
+} else {
+    # not supported / tested version of Powershell
+}
+
+# Double check if the module is (properly installed)
+if (-NOT (Get-Module -ListAvailable -Name ImportExcel)){
+        Show-MessageboxError "Das benötigte Modul `"ImportExcel`" konnte nicht installiert werden. Überprüfen Sie die Internetverbindung und beachten Sie die roten Fehlermeldungen." "Fehler bei Modulinstallation" | Out-Null
+        exit
+}
+
+# style information (can be done with Visual Studio - just paste the xaml content here in $inputXML)
 $inputXML = @"
 <Window x:Class="FF_Spenden_Helper.MainWindow"
         xmlns="http://schemas.microsoft.com/winfx/2006/xaml/presentation"
@@ -276,13 +416,13 @@ $xaml.SelectNodes("//*[@Name]") | %{Set-Variable -Name "WPF$($_.Name)" -Value $F
  
 Function Get-FormVariables{
 
-if ($global:ReadmeDisplay -ne $true){
-    Write-host "If you need to reference this display again, run Get-FormVariables" -ForegroundColor Yellow;$global:ReadmeDisplay=$true
-}
+    if ($global:ReadmeDisplay -ne $true){
+        Write-host "If you need to reference this display again, run Get-FormVariables" -ForegroundColor Yellow;$global:ReadmeDisplay=$true
+    }
 
-write-host "Found the following interactable elements from our form" -ForegroundColor Cyan
+    write-host "Found the following interactable elements from our form" -ForegroundColor Cyan
 
-get-variable WPF*
+    get-variable WPF*
 
 }
  
@@ -299,13 +439,16 @@ $ListDonators = @()
 #===========================================================================
 # Actually make the objects work
 #===========================================================================
+
+# Button for choosing Excel file with donators
 $WPFbtn_chooseExcelFile.Add_Click({
     $WPFtb_loadedFile.Text = Get-FileName ".\"
 })
 
+# Button for starting the SZR File generation
 $WPFbtn_GenerateSZRFile.Add_Click({
     if( -NOT (Test-Path $WPFtb_loadedFile.Text)) {
-        Show-MessageboxError "Es wurde keine gültige Excel-Datei ausgewählt. Bitte nochmals überprüfen.", "Keine Datei gewählt."
+        Show-MessageboxError "Es wurde keine gültige Excel-Datei ausgewählt. Bitte nochmals überprüfen." "Keine Datei gewählt."
         return
     }
     try {
@@ -324,17 +467,19 @@ $WPFbtn_GenerateSZRFile.Add_Click({
     }
 })
 
+# Button for choosing an SZR response archive
 $WPFbtn_chooseSZRFile.Add_Click({
     $WPFtb_loadedSZRFile.Text = Get-FileName ".\" "ZIP-Archiv (*.zip)| *.zip"
 })
 
+# Button for generating an Finanzonline XML
 $WPFbtn_GenerateFOFile.Add_Click({
     if( -NOT (Test-Path $WPFtb_loadedFile.Text)) {
-        Show-MessageboxError "Es wurde keine gültige Excel-Datei ausgewählt. Bitte nochmals überprüfen.", "Keine Datei gewählt."
+        Show-MessageboxError "Es wurde keine gültige Excel-Datei ausgewählt. Bitte nochmals überprüfen.", "Keine Excel-Datei gewählt."
         return
     }
     if( -NOT (Test-Path $WPFtb_loadedSZRFile.Text)) {
-        Show-MessageboxError "Es wurde keine SZR-Antwort-Datei ausgewählt. Bitte nochmals überprüfen.", "Keine Datei gewählt."
+        Show-MessageboxError "Es wurde keine SZR-Antwort-Datei ausgewählt. Bitte nochmals überprüfen.", "Keine SZR-Datei gewählt."
         return
     }
     
